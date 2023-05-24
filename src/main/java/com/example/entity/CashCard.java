aa63dc01d95a9cee46b3f5c80de3caae74c6b6a9
bolu <bolu.olubajo@and.digital>
2023-05-24 14:33:05 +0100

    Replaced grouped imports with single line imports

--- src/main/java/com/example/entity/CashCard.java
+++ src/main/java/com/example/entity/CashCard.java
@@ -1,6 +1,9 @@
 package com.example.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class CashCard {
--- src/main/java/com/example/service/CashCardService.java
+++ src/main/java/com/example/service/CashCardService.java
@@ -48,25 +48,3 @@ public class CashCardService {
         return "Update failed!";
     }
 }
-
-// Build data layer to return from an in-memory db or actual db (postgressSQL, mongoDb, h2) use relational dbm DONE
-// Set up cash card repository DONE
-// Write test cases both positive and negative scenarios
-// Think of all the exceptions that the service could return. Error codes in case of an error e.g cc001
-// error codes that would be exposed by my service
-// How to properly handle exceptions.
-// Service unavailable(internal server error) must be avoided by all cause
-// When sending a service that is in correct return a bad request
-// Sending a valid request and still the server is not able to handle it correctly.
-
-// 400 - BAD Request (priority)
-// 503 - Service not available
-// Enforce contract that says if your request is bad,
-// Minimise Internal server error
-// Handle exceptions gracefully
-// Anticipate exceptions and handle it
-// Try catch block to log exceptions
-// API should not expose internals of your system.
-// B2B and B2C. Careful if your application is B2C
-// Everything should be in a good state before hosting in a remote environment.
-// Write test for all sce
