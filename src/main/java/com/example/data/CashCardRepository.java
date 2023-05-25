d993e24ba6bc6bad578b1346270cfdbd687de3b9
bolu <bolu.olubajo@and.digital>
2023-05-25 16:04:13 +0100

    Added service tests

--- src/main/java/com/example/data/CashCardRepository.java
+++ src/main/java/com/example/data/CashCardRepository.java
@@ -2,6 +2,8 @@ package com.example.data;
 
 import com.example.entity.CashCard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CashCardRepository extends JpaRepository<CashCard, Long> {
 }
